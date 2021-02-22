dd7db551bca87ec6a4e2e35bcfd501134f41e780
vrushabh95 <mundhe.vrushabh@gmail.com>
2021-02-22 15:36:04 +0530

    Refactored : Removed Print Statment

--- BasicCorePrograms/ReplaceString.py
+++ BasicCorePrograms/ReplaceString.py
@@ -2,7 +2,7 @@
 
 #1.user Input and Replace String Template “Hello <<UserName>>, How are you?” 
 
-temp="Hello <<UserName>>, How are you?"
-name=input("Enter username\n")
-temp=temp.replace("<<UserName>>",name)
-print(temp)
+temp = "Hello <<UserName>>, How are you?"
+name = input("Enter username\n")
+temp = temp.replace("<<UserName>>", name)
+
