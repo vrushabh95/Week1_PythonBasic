afc748f33470c8c77f0c867e143d71a098fc566d
vrushabh95 <mundhe.vrushabh@gmail.com>
2021-02-24 07:55:52 +0530

    Refactored : used regular expression

--- BasicCorePrograms/ReplaceString.py
+++ BasicCorePrograms/ReplaceString.py
@@ -1,8 +1,15 @@
+import re
 
+#1.user Input and Replace String Template “Hello <<UserName>>, How are you?”
 
-#1.user Input and Replace String Template “Hello <<UserName>>, How are you?” 
+letter = "Hello <<UserName>>, How are you?"
+name = (input("Enter username\n"))
+if not re.match("^[a-zA-z]*$", name):
+    print("Error! only letters a-z allowed")
+else:
 
-temp = "Hello <<UserName>>, How are you?"
-name = input("Enter username\n")
-temp = temp.replace("<<UserName>>", name)
-
+    if (len(name) >= 3):
+        letter = letter.replace("<<UserName>>", name)
+        print(letter)
+    else:
+        print("the given username less than 3")
